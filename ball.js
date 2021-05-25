1a7d1f6d733a86ecdd912799c7ac8a017d6ccf73
sejalsuryavanshi <75065798+sejalsuryavanshi@users.noreply.github.com>
2021-05-25 13:14:03 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,30 @@
+class Ball{
+
+    constructor(x,y,width,height,angle){
+
+        var options={
+            frictionAir:0.005,
+            density:1.2
+        }
+
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        this.width=width;
+        this.height=height;
+
+        World.add(world,this.body);
+    }
+
+    display(){
+
+        var angle=this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x,pos.y)
+        rotate(angle)
+        ellipseMode(CENTER);
+        stroke("black");
+        fill("purple");
+        ellipse(0,0,this.width,this.height)
+        pop();
+    }
+}
---
+++ box.js
@@ -0,0 +1,39 @@
+class Box{
+
+    constructor(x,y,width,height){
+
+        var options={
+            restituition:1.0,
+            friction:0.8,
+            density:1.2,
+        }
+
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        this.width=width;
+        this.height=height;
+
+        World.add(world,this.body);
+    }
+
+    display(){
+
+        var angle=this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x,pos.y)
+        rotate(angle)
+        rectMode(CENTER);
+        stroke("Brown");
+        fill("LightBlue");
+        rect(0,0,this.width,this.height)
+        pop();
+    }
+
+
+
+
+
+
+
+
+}
